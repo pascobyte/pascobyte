17c5bed62308755a37295935a79c13b4ff364b63
pascobyte <127855307+pascobyte@users.noreply.github.com>
2023-07-24 21:13:07 +0200

    fixed checking js by purgecss

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    content: ["./public/**/*.html"], // The path to your compiled HTML files
-    css: ["./public/**/*.css"], // The path to your compiled CSS files
-    output: "./public/css" // The output directory for the optimized CSS files
-  };
-  
+  content: ["./public/**/*.html", "./public/**/*.js"], // Include paths to HTML and JS files
+  css: ["./public/**/*.css"], // The path to your compiled CSS files
+  output: "./public/css" // The output directory for the optimized CSS files
+};
